cb4ea864098443bb70af84c9a594ab397065f80e
Rahmaherat <rahmaherat2@gmail.com>
2024-11-03 21:05:35 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,8 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import React from 'react';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,6 +13,9 @@ function App() {
       return (
           <div>
               <WelcomeMessage />
+              <Header />
+            <MainContent />
+            <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
