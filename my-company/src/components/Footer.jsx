db1323426507fbcc4175878cd92238ee99a6327d
Rahmaherat <rahmaherat2@gmail.com>
2024-11-09 23:37:18 +0200

    Create Footer.jsx

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: '#fff', padding: '10px', textAlign: 'center' }}>
+      <p>&copy; 2023 My Company</p>
+    </footer>
+  );
+}
+
+export default Footer;
