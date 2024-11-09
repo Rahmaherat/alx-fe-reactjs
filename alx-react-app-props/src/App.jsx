580034bcb56753ad94a5bc6b6704983fc9d3ae37
Rahmaherat <rahmaherat2@gmail.com>
2024-11-09 23:13:51 +0200

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import ProfilePage from './ProfilePage';
-import { UserProvider } from './UserContext'; // Import UserProvider
+import ProfilePage from './ProfilePage'; 
+import { UserProvider } from './UserContext';  // Import the UserProvider component
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" }; // Example user data
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };  // Example user data
 
   return (
-    <UserProvider userData={userData}> {/* Wrap ProfilePage with UserProvider */}
-      <ProfilePage />
+    <UserProvider userData={userData}>  {/* Wrap ProfilePage with UserProvider */}
+      <ProfilePage />  {/* ProfilePage is now able to access userData via context */}
     </UserProvider>
   );
 }
 
 export default App;
+
