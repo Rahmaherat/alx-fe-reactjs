390fe154e35bf3c286b123708350198efb71c7c7
Rahmaherat <rahmaherat2@gmail.com>
2024-11-09 22:55:39 +0200

    Create  App.jsx

---
+++ alx-react-app-new/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './components/Counter';  // Import the Counter component
+
+function App() {
+  return (
+    <div>
+      <Counter />  {/* Render the Counter component */}
+    </div>
+  );
+}
+
+export default App;
