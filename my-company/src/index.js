a508dc4e557fd6e5b6a3d4f70d91578db8b60ea3
Rahmaherat <rahmaherat2@gmail.com>
2024-11-09 23:40:10 +0200

    Update index.js

--- my-company/src/index.js
+++ my-company/src/index.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Optional, for global styles (if any)
+import App from './App'; // Import the App component
+import { BrowserRouter as Router } from 'react-router-dom'; // Import Router for routing
+
+// The root element where React will render the app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component inside Router
+root.render(
+  <Router>
+    <App /> {/* The main app component */}
+  </Router>
+);
 
