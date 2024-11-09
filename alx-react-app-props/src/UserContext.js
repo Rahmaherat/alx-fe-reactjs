620a3689ac13cf606b86678e28cdda83b9a9faad
Rahmaherat <rahmaherat2@gmail.com>
2024-11-09 23:05:30 +0200

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -5,15 +5,15 @@ const UserContext = createContext();
 
 // Create a custom hook to use the UserContext
 export const useUser = () => {
-    return useContext(UserContext);  // Access the context value
+  return useContext(UserContext);
 };
 
 // Create a provider component that wraps the app and provides the user data
 export const UserProvider = ({ children, userData }) => {
-    return (
-        <UserContext.Provider value={userData}>
-            {children}  {/* All child components will now have access to userData */}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider value={userData}>
+      {children} {/* All child components will now have access to userData */}
+    </UserContext.Provider>
+  );
 };
 
