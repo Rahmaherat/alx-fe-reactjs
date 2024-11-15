6c18a6760a2f0c9b289db8ad0900dce0c07ca747
Rahmaherat <rahmaherat2@gmail.com>
2024-11-15 15:50:40 +0200

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,26 +1,33 @@
-// src/components/RecipeList.jsx
+// src/components/RecipeDetails.jsx
 import { useRecipeStore } from '../recipeStore';
+import { Link, useParams } from 'react-router-dom';
+import EditRecipeForm from './EditRecipeForm';
+import DeleteRecipeButton from './DeleteRecipeButton';
 
-const RecipeList = () => {
-  // Accessing the recipes state from the Zustand store
-  const recipes = useRecipeStore((state) => state.recipes);
+const RecipeDetails = () => {
+  const { recipeId } = useParams(); // Get the recipe ID from the URL params
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((recipe) => recipe.id === parseInt(recipeId))
+  );
+
+  if (!recipe) {
+    return <div>Recipe not found!</div>;
+  }
 
   return (
     <div>
-      <h2>Recipe List</h2>
-      {recipes.length === 0 ? (
-        <p>No recipes yet! Add some recipes.</p>
-      ) : (
-        recipes.map((recipe) => (
-          <div key={recipe.id} style={{ marginBottom: '15px' }}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))
-      )}
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+
+      <div>
+        <Link to={`/edit/${recipe.id}`}>
+          <button>Edit Recipe</button>
+        </Link>
+        <DeleteRecipeButton recipeId={recipe.id} />
+      </div>
     </div>
   );
 };
 
-export default RecipeList;
+export default RecipeDetails;
 
