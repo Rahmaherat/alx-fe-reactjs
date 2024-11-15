0aef87840d0c09a04c15ee5924917ea20c9838de
Rahmaherat <rahmaherat2@gmail.com>
2024-11-15 11:07:18 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -1,1 +1,13 @@
+// src/recipeStore.js
+import create from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],  // Initialize an empty array for recipes
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, newRecipe],
+  })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export { useRecipeStore };
 
