b2797e2b30bcb4b46fb0f9cc6ac86246f94f294d
Rahmaherat <rahmaherat2@gmail.com>
2024-11-15 16:16:46 +0200

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,23 +1,17 @@
-import { useState } from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import RecipeDetails from './components/RecipeDetails';
 import EditRecipeForm from './components/EditRecipeForm';
-import SearchBar from './components/SearchBar';
-import './App.css' 
-
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
     <Router>
       <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
         <h1>Recipe Sharing App</h1>
-
-        {/* Search Bar */}
-        <SearchBar />
 
         {/* Recipe Form */}
         <AddRecipeForm />
@@ -30,12 +24,17 @@ function App() {
           <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
           <Route path="/edit/:recipeId" element={<EditRecipeForm />} />
         </Routes>
+
+        {/* Favorites and Recommendations */}
+        <FavoritesList />
+        <RecommendationsList />
       </div>
     </Router>
   );
 }
 
 export default App;
+
 
 
 
