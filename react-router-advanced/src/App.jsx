7dbbcc685a9deda2fdc79937252f874131fabb9d
Rahmaherat <rahmaherat2@gmail.com>
2024-11-24 23:43:56 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { AuthProvider } from 'AuthContext'; // Import AuthProvider
 import Home from './components/Home';            // Home component
+import Profile from './components/Profile';
+import ProfileDetails from './components/ProfileDetails';
+import ProfileSettings from './components/ProfileSettings';
+import UserProfile from './components/UserProfile';
 import BlogPost from './components/BlogPost';    // BlogPost component (to be created)
 import NotFound from './components/NotFound';    // 404 Not Found component
 
@@ -29,6 +33,28 @@ function App() {
         </nav>
 
         <Routes>
+              {/* Public Route */}
+          <Route path="/" element={<Home />} />
+
+          {/* Protected Route */}
+          <Route 
+            path="/profile" 
+            element={
+              <ProtectedRoute> {/* Protect the /profile route */}
+                <Profile />
+              </ProtectedRoute>
+            }
+          >
+            {/* Nested Routes under /profile */}
+            <Route path="details" element={<ProfileDetails />} />
+            <Route path="settings" element={<ProfileSettings />} />
+          </Route>
+
+          {/* Dynamic Route */}
+          <Route path="/user/:username" element={<UserProfile />} />
+
+          {/* Catch-all for 404 */}
+          <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Home />} />
           
           {/* Dynamic route for Blog Post */}
