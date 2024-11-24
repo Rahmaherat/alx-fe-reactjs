f0f92e8893c6110e7038a8069d4f70ed5e1bc885
Rahmaherat <rahmaherat2@gmail.com>
2024-11-24 14:12:56 +0200

    Update App.jsx

--- form-handling-react/src/components/App.jsx
+++ form-handling-react/src/components/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
 
 function App() {
   return (
     <div>
-      <h1>Registration Form</h1>
-      <RegistrationForm />
+      <h1>Registration Form with Formik</h1>
+      <FormikForm />
     </div>
   );
 }
 
 export default App;
+
