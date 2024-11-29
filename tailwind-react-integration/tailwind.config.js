5e52a89214fc483ecb0de8db09d0198ad1c35140
rahma herat <rahmaherat2@gmail.com>
2024-11-30 01:20:32 +0200

    Describe Changes

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  darkMode: false, // Optional: You can set this to 'media' or 'class' for dark mode support
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
