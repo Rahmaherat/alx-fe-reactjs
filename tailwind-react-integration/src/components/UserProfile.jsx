f05639e4721d678d2e5f846a90a7cfc41cccb9ac
Rahmaherat <rahmaherat2@gmail.com>
2024-11-30 02:44:01 +0200

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 md:max-w-sm max-w-xs sm:w-24 sm:h-24 sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       {/* Profile Image with hover effect */}
       <img
         src="https://via.placeholder.com/150"
@@ -22,6 +22,7 @@ function UserProfile() {
 }
 
 export default UserProfile;
+
 
 
 
