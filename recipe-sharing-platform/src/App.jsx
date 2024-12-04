5876e2351823d281763fe0c5c4a94fab0ecd7111
rahma herat <rahmaherat2@gmail.com>
2024-12-04 16:49:45 +0200

    Initial setup of React app with Tailwind CSS

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+  function App() {
+    return (
+      <div className="App">
+        <h1 className="text-blue-500 text-4xl font-bold">
+          Welcome to Recipe Sharing Platform!
+        </h1>
+      </div>
+    );
+  }
+  
+  export default App;
+  
+
+
